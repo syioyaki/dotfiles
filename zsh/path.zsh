1d4b0ebbda3ea595a15b745080cdbdcba70d5cd6
syioyaki <shioyakizakana@gmail.com>
2026-07-04 13:34:19 +0900

    update(zshrc):pathを別ファイルとして作成した

---
+++ zsh/path.zsh
@@ -0,0 +1,28 @@
+export PYENV_ROOT="${HOME}/.pyenv"
+export DENO_INSTALL="{$HOME}/.deno"
+export NIVM_DIR="/opt/nvim-linux-arm64"
+$PATH+=(
+  /snap/bin(N-/)
+  $HOME/.local/bin(N-/)
+  $NVIM_DIR/bin(N-/)
+  $HOME/bin/chezmoi(N-/)
+  $HOME/.nvm(N-/)
+  $DENO_INSTALL/bin(N-/)
+  $PYENV_ROOT/bin(N-/)
+  $HOME/.cargo/env(N-/)
+  
+)
+
+if type nvim > /dev/null; then
+  export EDITOR=nvim
+else
+  export EDITOR=vim
+fi
+
+if [ -d "${PYENV_ROOT}" ]; then
+ eval "$(pyenv init -)"
+ eval "$(pyenv virtualenv-init -)"
+fi
+
+# 重複排除とパスの順序維持
+typeset -U path PATH
