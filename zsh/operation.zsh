d2f0238988025fbcd601972cf5600facdc25ac25
syioyaki <shioyakizakna@gmail.com>
2026-08-16 16:35:00 +0900

    add(zsh):zshの操作と履歴に関わる設定ファイルを追加した

---
+++ zsh/operation.zsh
@@ -0,0 +1,29 @@
+# ============================================================
+# operation
+# ============================================================
+
+# ------------------------------------------------------------
+# 操作
+# ------------------------------------------------------------
+setopt no_beep # 音を鳴らさない
+setopt IGNOREEOF # Ctrl+Dでログアウトしてしまうことを防ぐ
+bindkey -e
+autoload -Uz smart-insert-last-word
+# [a-zA-Z], /, \ のうち少なくとも1文字を含む長さ2以上の単語
+zstyle :insert-last-word match '*([[:alpha:]/\\]?|?[[:alpha:]/\\])*'
+zle -N insert-last-word smart-insert-last-word
+bindkey '^]' insert-last-word
+bindkey "^U" backward-kill-line
+bindkey "^C" send-break
+# Ctrl + s の停止を無効化
+stty stop undef
+
+# ------------------------------------------------------------
+# 履歴
+# ------------------------------------------------------------
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt share_history
+HISTFILE=$HOME/.zsh-history
+HISTSIZE=10000
+SAVEHIST=10000
